dd1ae2738006906deb7ae9c6f5dc9f8f24fb701b
Troyer05 <markusm05@icloud.com>
2024-11-24 04:19:20 +0100

    updated readme, added function comments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # C_framework_lib
 This is a C library that contains some functionalities as simple functions just like a framework.
+<br><br>
+
+# Functions 
+- CLS();
+- CLS_ENTER();
+- getUserInput(int line);
+- getInput();
--- framework.c
+++ framework.c
@@ -5,6 +5,9 @@
 
 #define INITIAL_BUFFER_SIZE 64
 
+/**
+ * Function to clear console
+ */
 void CLS() {
     #ifdef _WIN32
         system("cls");
@@ -13,6 +16,9 @@ void CLS() {
     #endif
 }
 
+/**
+ * Function to clear console after user hittet ENTER
+ */
 void CLS_ENTER() {
     sleep(2);
     getchar();
@@ -24,6 +30,12 @@ void CLS_ENTER() {
     #endif
 }
 
+/**
+ * Function to get user Inputs (desgined to be used in a loop for multi inputs)
+ * 
+ * @param int line Line Number (in for loop it would be i+1)
+ * @return char* the user input
+ */
 char* userInput(int line) {
     size_t bufferSize = INITIAL_BUFFER_SIZE;
     char* input = (char*)malloc(bufferSize);
@@ -59,6 +71,11 @@ char* userInput(int line) {
     return input;
 }
 
+/**
+ * Function to get simple oneline user input
+ * 
+ * @return char* the user input
+ */
 char* getInput() {
     size_t bufferSize = INITIAL_BUFFER_SIZE;
     char* input = (char*)malloc(bufferSize);
